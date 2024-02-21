cce761fb79a26f4c9443e0aaf83516c6793beee7
Lindsey-R <105560190+Lindsey-R@users.noreply.github.com>
2024-02-21 15:56:48 -0600

    At #20: results does not capture what I want

--- R/week6.R
+++ R/week6.R
@@ -14,4 +14,7 @@ mean(str_length(citations_txt))
 # Data Cleaning
 
 
-citations_tbl <- tibble(line = 1:length(citations_txt), cite = )
+citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
+  mutate(cite = str_replace_all(cite, pattern = "[\'\"]", replacement = "")) %>%
+  mutate(year = str_extract(cite, pattern = "\\d{4}")) %>%
+  mutate(page_start = str_extract(cite, pattern = "(\\d+)-\\d+"))
